12041351acde9ff8851482ec5142999bd7822002
Elizaveta <lisa-bella97@mail.ru>
2017-12-11 00:46:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ make
 if [ -f "/usr/local/bin/crack" ]; then 
     rm /usr/local/bin/crack
 fi
-ln ./Cipher_crack /usr/local/bin/crack
+sudo ln ./Cipher_crack /usr/local/bin/crack
 cd ..
-cp crack.1.gz /usr/share/man/man1/crack.1.gz
+sudo cp crack.1.gz /usr/share/man/man1/crack.1.gz
